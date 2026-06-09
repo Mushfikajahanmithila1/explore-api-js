9fe4c5a90ac05950bc6878d7f467e0f68d49de5e
mushfika <musfika962@gmail.com>
2026-06-09 19:46:30 +0600

    added

---
+++ comments.js
@@ -0,0 +1,17 @@
+function loadComments() {
+  fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+}
+loadComments();
+
+const loadComments2 = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
